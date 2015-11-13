d68b78b7b423da13edd8a674c1092bb60c0bca87
vlad-ignatov <vlad.ignatov@gmail.com>
2015-11-13 14:57:46 +0200

    Auto-detect file types

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,19 +1,60 @@
 import { PropTypes, Component } from 'react';
+import { default as fs } from 'fs';
+import { appStateStore } from '../stores/AppStateStore';
 
 export default class Editor extends Component
 {
+    static propTypes = {
+        filePath : PropTypes.string
+    };
+
+    static defaultProps = {
+        filePath : ''
+    };
+
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            filePath : props.filePath || ''
+        };
+        this._onChange = this._onChange.bind(this);
+    }
+
+    componentWillUnmount()
+    {
+        appStateStore.removeChangeListener(this._onChange);
+    }
+
+    _onChange()
+    {
+        var path  = appStateStore.getState().selectedFilePath;
+        var isDir = fs.statSync(path).isDirectory();
+        if (!isDir) {
+            try {
+                let modelist = ace.require("ace/ext/modelist");
+                let mode     = modelist.getModeForPath(path).mode
+                this.editor.session.setMode(mode) // mode now contains "ace/mode/javascript".
+                this.editor.setValue(fs.readFileSync(path, 'utf8'), -1);
+            } catch(ex) {
+                console.error(ex);
+                this.editor.setValue('');
+            }
+        }
+    }
+
     componentDidMount()
     {
-        var editor = ace.edit(this.refs.wrapper);
-        editor.setTheme("ace/theme/twilight");
-        editor.getSession().setMode("ace/mode/jsx");
-        editor.setDisplayIndentGuides(false);
+        appStateStore.addChangeListener(this._onChange);
+        this.editor = ace.edit(this.refs.wrapper);
+        this.editor.$blockScrolling = Infinity;
+        this.editor.setTheme("ace/theme/twilight");
+        this.editor.getSession().setMode("ace/mode/jsx");
+        this.editor.setDisplayIndentGuides(false);
     }
 
     render()
     {
-        return (
-            <div ref="wrapper" id="editor"></div>
-        );
+        return (<div ref="wrapper" id="editor"/>);
     }
 }
