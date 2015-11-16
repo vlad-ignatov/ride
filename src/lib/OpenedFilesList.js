e4c813e21d60835481ce8e979ab98600f95ccbba
vlad-ignatov <vlad.ignatov@gmail.com>
2015-11-16 23:12:34 +0200

    New OpenedFilesList class to abstract opened files operations

---
+++ src/lib/OpenedFilesList.js
@@ -0,0 +1,12 @@
+
+/**
+ * Represents a collection OpenedFile objects (visually represented as tabs in
+ * the editor)
+ */
+export default class OpenedFilesList
+{
+    constructor()
+    {
+        this.files = [];
+    }
+}
