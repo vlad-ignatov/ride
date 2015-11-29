1c8cb68c17b7e95bb233ea9d78717be3c7f9ca68
vlad-ignatov <vlad.ignatov@gmail.com>
2015-11-29 14:38:16 +0200

    Implement custom "prompt" functionality

--- src/window.jsx
+++ src/window.jsx
@@ -10,29 +10,22 @@ window.alt = alt;
 
 // Proxy comands from the main process app to the window
 // ------------------------------------------------------------------------------
-ipc.on('setSyntaxTheme', configActions.setEditorTheme)
-ipc.on('toggleFileTree', configActions.toggleSidebarVisible)
-ipc.on('fontIncrease'  , configActions.increaseFontSize)
-ipc.on('fontDecrease'  , configActions.decreaseFontSize)
-ipc.on('saveFile'      , fileActions.save)
+ipc.on('setSyntaxTheme', configActions.setEditorTheme);
+ipc.on('toggleFileTree', configActions.toggleSidebarVisible);
+ipc.on('fontIncrease'  , configActions.increaseFontSize);
+ipc.on('fontDecrease'  , configActions.decreaseFontSize);
+ipc.on('saveFile'      , fileActions.save);
 ipc.on('openFiles', files => {
-    files.forEach(f => fileActions.openFile(f))
-})
+    files.forEach(f => fileActions.openFile(f));
+});
 ipc.on('fluxAction', function(actionsClass, actionName, ...rest) {
-    console.log('fluxAction handler args: ', arguments)
+    console.log('fluxAction handler args: ', arguments);
     try {
-        alt.actions[actionsClass][actionName](...rest)
+        alt.actions[actionsClass][actionName](...rest);
     } catch (ex) {
-        console.error(ex)
+        console.error(ex);
     }
 });
-
-
-
-
-
-
-
 
 $(function() {
     $(document).on('selectstart', false);
@@ -44,14 +37,14 @@ $(function() {
     // ------------------------------------------------------------------------------
     var pkg = require('../package.json');
     for (var name in pkg.ride.packages) {
-        let ext
+        let ext;
         try {
-            ext = require('../node_modules/' + name + '/ride-main-window.js')
+            ext = require('../node_modules/' + name + '/ride-main-window.js');
             if (typeof ext == 'function') {
-                ext.call(window)
+                ext.call(window);
             }
         } catch (ex) {
-            console.error(ex)
+            console.error(ex);
         }
     }
 
@@ -98,23 +91,23 @@ $(function() {
     // native context menus and popup menus -------------------------------------
     window.addEventListener('contextmenu', function(e) {
         if (!e.menuTemplate) {
-            e.menuTemplate = []
+            e.menuTemplate = [];
         }
     }, true);
 
     window.addEventListener('click', function(e) {
         if (!e.menuTemplate) {
-            e.menuTemplate = []
+            e.menuTemplate = [];
         }
     }, true);
 
     window.addEventListener('contextmenu', function(e) {
         if (!e.defaultPrevented && e.menuTemplate && e.menuTemplate.length) {
-            setTimeout(() => {
+            // setTimeout(() => {
                 Menu.buildFromTemplate(e.menuTemplate).popup(
                     remote.getCurrentWindow()
                 );
-            }, 50)
+            // }, 50);
         }
     }, false);
 
@@ -125,7 +118,62 @@ $(function() {
                 Menu.buildFromTemplate(e.menuTemplate).popup(
                     remote.getCurrentWindow()
                 );
-            }, 50)
+            }, 50);
         }
     }, false);
+
+    // Prompt ------------------------------------------------------------------
+    window.prompt = function prompt(msg, value) {
+        return new Promise(function(resolve, reject) {
+            var $prompt = $('#prompt');
+            if (!$prompt.length) {
+                $prompt = $(
+                    `<div id="prompt" class="dialog" style="display: none">
+                        <div class="prompt-message"></div>
+                        <input type="text"/>
+                    </div>`
+                )
+                .css({
+                    position: 'absolute',
+                    zIndex  : 1000,
+                    top     : 60,
+                    left    : '20%',
+                    width   : '60%'
+                })
+                .appendTo('.main-stage');
+            }
+
+            $prompt.find('.prompt-message').html(msg || '');
+            $prompt.show();
+            $prompt.find('input')
+            .val(value || '')
+            .one('blur', function() {
+                $(this).off();
+                $prompt.hide();
+                reject('No path selected');
+            })
+            .on('keydown', function(e) {
+                var val = this.value.trim();
+                if (val && e.keyCode == 13) {
+                    $(this).off().trigger('blur');
+                    $prompt.hide();
+                    resolve(val);
+                }
+                else if (e.keyCode == 27) {
+                    $(this).off().trigger('blur');
+                    $prompt.hide();
+                    reject('Action canceled');
+                }
+            });
+
+            setTimeout(() => {
+                let input = $prompt.find('input').trigger('focus'),
+                    len   = input.val().length;
+                input[0].selectionStart = len;
+                input[0].selectionEnd = len;
+                input.trigger('click');
+            }, 20);
+        });
+    };
+
 });
